05c9273daa2a0b27ea65b27c4c8c2f0fddf65ff5
MarcosRZ <msucaro@gmail.com>
2025-01-16 13:35:11 +0100

    Fix RSS feed schema

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,14 @@ export async function GET(context) {
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')).then(items => items.map(({ author, ...item }) => item)),
+    customData: `
+      <language>en-us</language>
+      <atom10:link
+        xmlns:atom10="http://www.w3.org/2005/Atom"
+        rel="self"
+        type="application/rss+xml"
+        href="https://marcosrz-astro-blog.netlify.app/rss.xml"
+      />`,
   });
 }
